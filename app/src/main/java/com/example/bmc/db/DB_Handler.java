d444887b1126e32b171b78fd1f6fcc86804bbf0f
Seagold8930 <41976302+Seagold8930@users.noreply.github.com>
2019-09-03 14:09:05 +1200

    Database connectivity

--- app/src/main/java/com/example/bmc/db/DB_Handler.java
+++ app/src/main/java/com/example/bmc/db/DB_Handler.java
@@ -4,12 +4,40 @@ import com.example.bmc.auxiliary.Building;
 import com.example.bmc.auxiliary.ComplianceInspection;
 import com.example.bmc.auxiliary.User;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
-public class DB_Handler {
 
-    public boolean login( String username, String password ) {
+public class DB_Handler {
+    private Connection conn = null;
+
+    public DB_Handler() {
+        String connectionString = "jdbc:sqlserver://bmcs.database.windows.net:1433;database=Building Management and Compliance;user=bmcs_admin;password=Weltec2019;encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        String username = "bmcs_admin";
+        String password = "Weltec2019";
+
+        try {
+            conn = DriverManager.getConnection( connectionString );
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean login(String username, String password ) {
         //TODO implement user validation logic
+        try {
+            PreparedStatement statement = conn.prepareStatement( "SELECT * FROM [dbo].[User] WHERE userID = ?" );
+            statement.setString( 1, username );
+            statement.execute();
+
+//            Statement statement = conn.createStatement();
+//            statement.execute( "SELECT * FROM User WHERE user.userID = ?" );
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
         return false;
     }
 
